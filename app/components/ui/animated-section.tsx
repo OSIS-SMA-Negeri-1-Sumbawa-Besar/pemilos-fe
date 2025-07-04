84aa6f821463e18ff209febfc3b06d6c7d5050eb
Rexadell <reksaparamaaditya@gmail.com>
2025-07-04 19:16:43 +0800

    visi misi section

---
+++ app/components/ui/animated-section.tsx
@@ -0,0 +1,41 @@
+'use client'
+/* eslint-disable react/prop-types */
+/* eslint-disable react/react-in-jsx-scope */
+import { motion, useInView } from 'framer-motion'
+import { useRef } from 'react'
+
+interface AnimatedSectionProps {
+  id?: string
+  children: React.ReactNode[] | React.ReactNode
+  className?: string
+}
+
+export const AnimatedSection: React.FC<AnimatedSectionProps> = ({
+  children,
+  className,
+  ...props
+}) => {
+  const ref = useRef(null)
+  const inView = useInView(ref, { once: false })
+
+  return (
+    <motion.section
+      id={props.id}
+      ref={ref}
+      initial={{ scale: 0 }}
+      animate={{ scale: inView ? 1 : 0 }}
+      transition={{
+        delay: 0.2,
+        duration: 0.5,
+        type: 'spring',
+        stiffness: 260,
+        damping: 20,
+      }}
+      exit={{ scale: 0 }}
+      className={className}
+      {...props}
+    >
+      {children}
+    </motion.section>
+  )
+}
--- app/routes/_page._index.tsx
+++ app/routes/_page._index.tsx
@@ -4,6 +4,7 @@ import { Navbar } from '~/components/ui/navbar';
 import { LandingModule } from '~/modules/LandingModule';
 import { LandingAction } from '~/modules/LandingModule/action';
 import { LandingLoader } from '~/modules/LandingModule/loader';
+import { VisiMisi } from '~/modules/LandingModule/visi-misi';
 
 export async function loader(args: LoaderFunctionArgs) {
   return LandingLoader(args);
@@ -15,11 +16,12 @@ export async function action(args: ActionFunctionArgs) {
 
 export default function LandingPage() {
   return (
-    <div className="">
+    <div>
       <Background />
       <Navbar />
       <main className="px-10 lg:px-40 flex flex-col gap-10 overflow-hidden">
         <LandingModule />
+        <VisiMisi />
       </main>
     </div>
   );
